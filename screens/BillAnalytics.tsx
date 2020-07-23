500e66f15b47b58dbb27b17b8c3a6335fe958701
Bhaskar <bhaskarmitra0178@gmail.com>
2020-07-23 22:29:37 +0530

    Charts added

--- screens/BillAnalytics.tsx
+++ screens/BillAnalytics.tsx
@@ -1,72 +1,82 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Platform } from 'react-native';
-import {Asset} from 'expo-asset';
+import { AppLoading } from "expo";
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { VictoryPie, VictoryTheme, VictoryBar, VictoryChart } from "victory-native";
+import CustomisedFlatList from "../components/CustomisedFlatList";
+import { AppData } from "../constants/AppData";
+import { Card } from "../components/Card";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default class BillAnalytics extends Component<any,any> {
-    constructor(props: any) {
-        super(props);
-        //STEP 2 - Chart Data
-        const chartData = [
-          { label: "Venezuela", value: "290" },
-          { label: "Saudi", value: "260" },
-          { label: "Canada", value: "180" },
-          { label: "Iran", value: "140" },
-          { label: "Russia", value: "115" },
-          { label: "UAE", value: "100" },
-          { label: "US", value: "30" },
-          { label: "China", value: "30" }
-        ];
-        //STEP 3 - Chart Configurations
-        const chartConfig = {
-          type: "column2d",
-          width: "100%",
-          height: "400",
-          dataFormat: "json",
-          dataSource: {
-            chart: {
-              caption: "Countries With Most Oil Reserves [2017-18]",
-              subCaption: "In MMbbl = One Million barrels",
-              xAxisName: "Country",
-              yAxisName: "Reserves (MMbbl)",
-              numberSuffix: "K",
-              theme: "fusion"
-            },
-            data: chartData
-          }
-        };
-        this.state = chartConfig;
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
 
-    }
-      render() {
-        return (
-          <View style={styles.container}>
-            <Text style={styles.header}>A Column 2D Chart</Text>
-    
-            <View style={styles.chartContainer}>
-           
-            </View>
-          </View>
-        );
-      }
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-    
-        padding: 10
-      },
-    
-      header: {
-        fontWeight: "bold",
-        fontSize: 20,
-        textAlign: "center",
-        paddingBottom: 10
-      },
-    
-      chartContainer: {
-        height: 400,
-        borderColor: "#000",
-        borderWidth: 1
-      }
-    });
+export default function BillAnalytics(props: any) {
+
+
+  return (
+    <View style={styles.container}>
+        <ScrollView>
+            <Card>
+            <VictoryPie
+                    data={[
+                        { x: "Cats", y: 35 },
+                        { x: "Dogs", y: 40 },
+                        { x: "Birds", y: 55 }
+                    ]}
+                />
+            </Card>
+            <Card>
+            <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={{ x: 15 }}
+                >
+                <VictoryBar
+                    barRatio={0.8}
+                    style={{
+                    data: { fill: "#c43a31" }
+                    }}
+                    data={[
+                        { x: "Cats", y: 35 },
+                        { x: "Dogs", y: 40 },
+                        { x: "Birds", y: 55 }
+                    ]}
+                />
+                </VictoryChart>
+            </Card>
+            <Card>
+            <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={{ x: 15 }}
+                >
+                <VictoryBar
+                    barRatio={0.8}
+                    style={{
+                    data: { fill: "#c43a31" }
+                    }}
+                    data={[
+                        { x: "Cats", y: 35 },
+                        { x: "Dogs", y: 40 },
+                        { x: "Birds", y: 55 }
+                    ]}
+                />
+                </VictoryChart>
+            </Card>
+        </ScrollView>
+     
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff",
+  },
+});
