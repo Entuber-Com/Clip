1c7bf1ef58c5843cbc66753645014864bc20798b
pdkumar96 <pdkumar96@users.noreply.github.com>
2020-08-01 15:55:44 -0400

    8/1 - 3:55

--- App.tsx
+++ App.tsx
@@ -8,6 +8,27 @@ import Navigation from './navigation';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import { MenuProvider } from 'react-native-popup-menu';
+import './hooks/Timer';
+import * as firebase from 'firebase';
+
+const firebaseConfig =  {
+    apiKey: "AIzaSyD4NZ1oc83NF5HswWXsWcJf3yN1Fcsx-Bg",
+    authDomain: "entuber.firebaseapp.com",
+    databaseURL: "https://entuber.firebaseio.com",
+    projectId: "entuber",
+    storageBucket: "entuber.appspot.com",
+    messagingSenderId: "431966848925",
+    appId: "1:431966848925:web:db6b793ad21b88da409174",
+    measurementId: "G-CXP8MYBDVF"
+};
+  
+try{
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);    
+    }
+} catch (error) {
+    console.log(error);
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
