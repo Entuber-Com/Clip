2cbf417b0bd8c51cd2d2a14d7f03a5036f324316
pdkumar96 <pdkumar96@users.noreply.github.com>
2020-08-01 23:34:04 -0400

    8/1-11:30

--- screens/ViewBill.tsx
+++ screens/ViewBill.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import { View, Dimensions, StyleSheet } from 'react-native';
-import PDFReader from 'rn-pdf-reader-js'
-import { Asset } from 'expo-asset';
+import { View, Dimensions, StyleSheet , Text} from 'react-native';
+import { WebView } from 'react-native-webview';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { FileSystem } from 'expo';
+import Colors from '../constants/Colors';
+import { Button } from 'native-base';
+/* import PDFReader from 'rn-pdf-reader-js'
+import { Asset } from 'expo-asset'; */
 
 const ViewBill = () => {
     return (
-        <View style={styles.container}>
-             <PDFReader
+        <View style={{flex: 1}}>
+            <WebView  source={{uri: 'https://firebasestorage.googleapis.com/v0/b/entuber.appspot.com/o/demoData%2FBill%20July%20CHUD.pdf?alt=media&token=702b0ed0-961f-4e25-b6f7-c03737f88726'}}/>
+         {/*     <PDFReader
                 style={styles.pdf}
-                source={{uri: Asset.fromModule(require('../assets/PDF/Bill_July_CHUD.pdf')).uri}}
-            />
+                source={{uri: Asset.fromModule(require('../assets/PDF/Bill_July_MDC.pdf')).uri}}
+            /> */}
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-start',
@@ -27,5 +33,6 @@ const styles = StyleSheet.create({
         height:Dimensions.get('window').height,
     }
 });
-
+ */
 export default ViewBill
+
